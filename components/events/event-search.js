126779b720b8b2a048ef9ca2ee6c1cef7b7aed62
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-03-25 16:24:21 +0700

    update: navigating to the filtered event page after click the button search event

--- components/events/event-search.js
+++ components/events/event-search.js
@@ -1,28 +1,41 @@
+import { useRef } from 'react';
+
 import Button from './../ui/button';
 import classes from './event-search.module.css';
 function EventSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
-            <option value="1">January</option>
-            <option value="2">Fabruary</option>
-            <option value="3">March</option>
-            <option value="4">April</option>
-            <option value="5">May</option>
-            <option value="6">June</option>
-            <option value="7">July</option>
-            <option value="8">August</option>
-            <option value="9">September</option>
+          <select id="month" ref={monthInputRef}>
+            <option value="01">January</option>
+            <option value="02">Fabruary</option>
+            <option value="03">March</option>
+            <option value="04">April</option>
+            <option value="05">May</option>
+            <option value="06">June</option>
+            <option value="07">July</option>
+            <option value="08">August</option>
+            <option value="09">September</option>
             <option value="10">October</option>
             <option value="11">November</option>
             <option value="12">December</option>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,19 @@
+import { useRouter } from 'next/router';
 import { getAllEvents } from './../../dummy-data';
 import EventList from './../../components/events/event-list';
 import EventSearch from './../../components/events/event-search';
 function AllEventPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  const searchEventHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>All Events</h1>
-      <EventSearch />
+      <EventSearch onSearch={searchEventHandler} />
       <EventList items={events} />
     </div>
   );
