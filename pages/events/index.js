703dfa1b030e4f4ce5f00c175fac68307180f6d4
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-03-25 16:12:43 +0700

    update: added ui search events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import { getAllEvents } from './../../dummy-data';
 import EventList from './../../components/events/event-list';
+import EventSearch from './../../components/events/event-search';
 function AllEventPage() {
   const events = getAllEvents();
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>All Events</h1>
+      <EventSearch />
       <EventList items={events} />
     </div>
   );
