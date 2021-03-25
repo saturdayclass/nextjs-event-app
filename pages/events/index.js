2a60ab24f5051eb33f4e1097a7465ebb00019ce1
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-03-25 15:59:22 +0700

    update: showing all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,11 @@
+import { getAllEvents } from './../../dummy-data';
+import EventList from './../../components/events/event-list';
 function AllEventPage() {
+  const events = getAllEvents();
   return (
     <div>
-      <h1>All Events</h1>
+      <h1 style={{ textAlign: 'center' }}>All Events</h1>
+      <EventList items={events} />
     </div>
   );
 }
