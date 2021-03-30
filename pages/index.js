a3fbe37dd5bfd0899a8b35a5cc5473210c96ae9b
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-03-30 16:41:57 +0700

    update: added helpers for better code

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { getFeaturedEvents } from './../dummy-data';
+import { getFeaturedEvents } from './../helpers/api-util';
 import EventList from './../components/events/event-list';
 function HomePage(props) {
   const { featuredEvents } = props;
@@ -12,22 +12,7 @@ function HomePage(props) {
 export default HomePage;
 
 export async function getStaticProps(context) {
-  const req = await fetch(
-    'https://nextjs-data-fetching-b085e-default-rtdb.firebaseio.com/events.json'
-  );
-  const res = await req.json();
-
-  const events = [];
-
-  for (const key in res) {
-    const event = {
-      id: key,
-      ...res[key],
-    };
-
-    events.push(event);
-  }
-  const featuredEvents = events.filter((event) => event.isFeatured === true);
+  const featuredEvents = await getFeaturedEvents();
 
   return {
     props: {
