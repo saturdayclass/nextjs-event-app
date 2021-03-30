bf178830c058806ac5581cc1d4764918426b5b73
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-03-30 16:18:36 +0700

    update: change dummy data in show featured events with serversideprops

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { getFeaturedEvents } from './../dummy-data';
 import EventList from './../components/events/event-list';
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+  const { featuredEvents } = props;
   return (
     <div>
       <EventList items={featuredEvents} />
@@ -10,3 +10,28 @@ function HomePage() {
 }
 
 export default HomePage;
+
+export async function getServerSideProps(context) {
+  const req = await fetch(
+    'https://nextjs-data-fetching-b085e-default-rtdb.firebaseio.com/events.json'
+  );
+  const res = await req.json();
+
+  const events = [];
+
+  for (const key in res) {
+    const event = {
+      id: key,
+      ...res[key],
+    };
+
+    events.push(event);
+  }
+  const featuredEvents = events.filter((event) => event.isFeatured === true);
+
+  return {
+    props: {
+      featuredEvents: featuredEvents,
+    },
+  };
+}
