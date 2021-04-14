061e6ce5a6c96461b1eeff6e5af509bb1f31b584
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-04-14 10:27:14 +0700

    update: added error handling

--- pages/api/newslatter.js
+++ pages/api/newslatter.js
@@ -1,4 +1,17 @@
 import { MongoClient } from 'mongodb';
+
+async function Connect() {
+  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_DB, {
+    useUnifiedTopology: true,
+  });
+  return client;
+}
+
+async function insertDocument(client, email) {
+  const db = await client.db();
+  await db.collection('newslatter').insertOne({ email });
+}
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email;
@@ -8,13 +21,20 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(process.env.NEXT_PUBLIC_DB, {
-      useUnifiedTopology: true,
-    });
-    const db = await client.db();
-    await db.collection('newslatter').insertOne({ email });
+    let client;
+    try {
+      client = await Connect();
+    } catch (err) {
+      res.status(500).json({ message: 'Connecting to the database failded!' });
+      return;
+    }
+    try {
+      await insertDocument(client, { email });
+      client.close();
+    } catch (err) {
+      res.status(500).json({ message: 'Inserting data failded!' });
+    }
 
-    client.close();
     res
       .status(201)
       .json({ message: 'Email was successfully registered', data: email });
