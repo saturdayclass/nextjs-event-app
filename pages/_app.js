02ab71bdcfb05ac313535ee4d120653bdf47bcec
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-04-14 16:09:17 +0700

    update: create react context

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,24 @@ import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 import Notification from './../components/ui/notification';
+import { NotificationContextProvider } from './../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Nextjs Event</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-      <Notification
-        title="Title"
-        message="This is notification"
-        status="success"
-      />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Nextjs Event</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+        <Notification
+          title="Title"
+          message="This is notification"
+          status="success"
+        />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
